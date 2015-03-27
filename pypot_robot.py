3655c022b72b3f8bead9f1a0e40b812c4b095bd1
tvigue <thomas.vigue@etu.u-bordeaux.fr>
2015-03-27 10:21:01 +0100

    ajout fonction de marche

--- pypot_robot.py
+++ pypot_robot.py
@@ -83,29 +83,19 @@ def allMotorCompliant(robot):
 		m.compliant = True
 
 def allMotorInitialPos(robot):
-	print "\n",pos_patte1[0],"\n"
-	setPositionToLeg(pos_patte1[0], pos_patte1[1], pos_patte1[2], robot.patte_1)
-	setPositionToLeg(pos_patte2[0], pos_patte2[1], pos_patte2[2], robot.patte_2)
-	setPositionToLeg(pos_patte3[0], pos_patte3[1], pos_patte3[2], robot.patte_3)
-	setPositionToLeg(pos_patte4[0], pos_patte4[1], pos_patte4[2], robot.patte_4)
-	setPositionToLeg(pos_patte5[0], pos_patte5[1], pos_patte5[2], robot.patte_5)
-	setPositionToLeg(pos_patte6[0], pos_patte6[1], pos_patte6[2], robot.patte_6)
+	global pos_patte1
+	global pos_patte2
+	global pos_patte3
+	global pos_patte4
+	global pos_patte5
+	global pos_patte6
 	
-def moveRobot(x,y,z,robot):
-	print "\n",pos_patte1[0],"\n"
-	pos_patte1_tmp=[pos_patte1[0]+y,pos_patte1[1]-x,pos_patte1[2]+z]
-	pos_patte2_tmp=[pos_patte2[0]-x,pos_patte2[1]-y,pos_patte2[2]+z]
-	pos_patte3_tmp=[pos_patte3[0]-x,pos_patte3[1]-y,pos_patte3[2]+z]
-	pos_patte4_tmp=[pos_patte4[0]-y,pos_patte4[1]+x,pos_patte4[2]+z]
-	pos_patte5_tmp=[pos_patte5[0]+x,pos_patte5[1]+y,pos_patte5[2]+z]
-	pos_patte6_tmp=[pos_patte6[0]+x,pos_patte6[1]+y,pos_patte6[2]+z]
-	
-	pos_patte1=pos_patte1_tmp
-	pos_patte2=pos_patte2_tmp
-	pos_patte3=pos_patte3_tmp
-	pos_patte4=pos_patte4_tmp
-	pos_patte5=pos_patte5_tmp
-	pos_patte6=pos_patte6_tmp
+	pos_patte1=[110,0,-60]
+	pos_patte2=[100,-100,-60]
+	pos_patte3=[100,100,-60]
+	pos_patte4=[110,0,-60]
+	pos_patte5=[100,-100,-60]
+	pos_patte6=[100,100,-60]
 	
 	setPositionToLeg(pos_patte1[0], pos_patte1[1], pos_patte1[2], robot.patte_1)
 	setPositionToLeg(pos_patte2[0], pos_patte2[1], pos_patte2[2], robot.patte_2)
@@ -113,6 +103,122 @@ def moveRobot(x,y,z,robot):
 	setPositionToLeg(pos_patte4[0], pos_patte4[1], pos_patte4[2], robot.patte_4)
 	setPositionToLeg(pos_patte5[0], pos_patte5[1], pos_patte5[2], robot.patte_5)
 	setPositionToLeg(pos_patte6[0], pos_patte6[1], pos_patte6[2], robot.patte_6)
+	
+def moveRobot(x, y, z, robot, up = 0):
+	global pos_patte1
+	global pos_patte2
+	global pos_patte3
+	global pos_patte4
+	global pos_patte5
+	global pos_patte6
+	
+	if up == 0:
+		pos_patte1 = [pos_patte1[0] + y, pos_patte1[1] - x, pos_patte1[2] + z]
+		pos_patte2 = [pos_patte2[0] - x, pos_patte2[1] - y, pos_patte2[2] + z]
+		pos_patte3 = [pos_patte3[0] - x, pos_patte3[1] - y, pos_patte3[2] + z]
+		pos_patte4 = [pos_patte4[0] - y, pos_patte4[1] + x, pos_patte4[2] + z]
+		pos_patte5 = [pos_patte5[0] + x, pos_patte5[1] + y, pos_patte5[2] + z]
+		pos_patte6 = [pos_patte6[0] + x, pos_patte6[1] + y, pos_patte6[2] + z]
+	elif up == 1:
+		pos_patte1 = [pos_patte1[0] + y, pos_patte1[1] - x, pos_patte1[2] + z]
+		pos_patte3 = [pos_patte3[0] - x, pos_patte3[1] - y, pos_patte3[2] + z]
+		pos_patte5 = [pos_patte5[0] + x, pos_patte5[1] + y, pos_patte5[2] + z]
+	elif up == 2:
+		pos_patte2 = [pos_patte2[0] - x, pos_patte2[1] - y, pos_patte2[2] + z]
+		pos_patte4 = [pos_patte4[0] - y, pos_patte4[1] + x, pos_patte4[2] + z]
+		pos_patte6 = [pos_patte6[0] + x, pos_patte6[1] + y, pos_patte6[2] + z]
+	
+	setPositionToLeg(pos_patte1[0], pos_patte1[1], pos_patte1[2], robot.patte_1)
+	setPositionToLeg(pos_patte2[0], pos_patte2[1], pos_patte2[2], robot.patte_2)
+	setPositionToLeg(pos_patte3[0], pos_patte3[1], pos_patte3[2], robot.patte_3)
+	setPositionToLeg(pos_patte4[0], pos_patte4[1], pos_patte4[2], robot.patte_4)
+	setPositionToLeg(pos_patte5[0], pos_patte5[1], pos_patte5[2], robot.patte_5)
+	setPositionToLeg(pos_patte6[0], pos_patte6[1], pos_patte6[2], robot.patte_6)
+	time.sleep(.2)
+	
+tmp = 0
+	
+def rotateRobot(angle, robot, up = 0):
+	global pos_patte1
+	global pos_patte2
+	global pos_patte3
+	global pos_patte4
+	global pos_patte5
+	global pos_patte6
+	
+	theta1 = math.atan2(pos_patte1[1], pos_patte1[0]) + math.radians(angle)
+	theta2 = math.atan2(pos_patte2[1], pos_patte2[0]) + math.radians(angle)
+	theta3 = math.atan2(pos_patte3[1], pos_patte3[0]) + math.radians(angle)
+	theta4 = math.atan2(pos_patte4[1], pos_patte4[0]) + math.radians(angle)
+	theta5 = math.atan2(pos_patte5[1], pos_patte5[0]) + math.radians(angle)
+	theta6 = math.atan2(pos_patte6[1], pos_patte6[0]) + math.radians(angle)
+	
+	print math.degrees(math.atan2(pos_patte1[1], pos_patte1[0])), " : ", math.degrees(theta1)
+	print math.degrees(math.atan2(pos_patte2[1], pos_patte2[0])), " : ", math.degrees(theta2)
+	print math.degrees(math.atan2(pos_patte3[1], pos_patte3[0])), " : ", math.degrees(theta3)
+	print math.degrees(math.atan2(pos_patte4[1], pos_patte4[0])), " : ", math.degrees(theta4)
+	
+	if up == 0:
+		pos_patte1 = [pos_patte1[0] * math.cos(theta1), pos_patte1[1] * math.sin(theta1), pos_patte1[2]]
+		pos_patte2 = [pos_patte2[0] * math.cos(theta2), pos_patte2[1] * math.sin(theta2), pos_patte2[2]]
+		pos_patte3 = [pos_patte3[0] * math.cos(theta3), pos_patte3[1] * math.sin(theta3), pos_patte3[2]]
+		pos_patte4 = [pos_patte4[0] * math.cos(theta4), pos_patte4[1] * math.sin(theta4), pos_patte4[2]]
+		pos_patte5 = [pos_patte5[0] * math.cos(theta5), pos_patte5[1] * math.sin(theta5), pos_patte5[2]]
+		pos_patte6 = [pos_patte6[0] * math.cos(theta6), pos_patte6[1] * math.sin(theta6), pos_patte6[2]]
+	elif up == 1:
+		pos_patte1 = [pos_patte1[0] * math.cos(theta1), pos_patte1[1] * math.sin(theta1), pos_patte1[2]]
+		pos_patte3 = [pos_patte3[0] * math.cos(theta3), pos_patte3[1] * math.sin(theta3), pos_patte3[2]]
+		pos_patte5 = [pos_patte5[0] * math.cos(theta5), pos_patte5[1] * math.sin(theta5), pos_patte5[2]]
+	elif up == 2:
+		pos_patte2 = [pos_patte2[0] * math.cos(theta2), pos_patte2[1] * math.sin(theta2), pos_patte2[2]]
+		pos_patte4 = [pos_patte4[0] * math.cos(theta4), pos_patte4[1] * math.sin(theta4), pos_patte4[2]]
+		pos_patte6 = [pos_patte6[0] * math.cos(theta6), pos_patte6[1] * math.sin(theta6), pos_patte6[2]]
+	
+	global tmp
+	
+	print tmp,"\n",pos_patte1
+	print pos_patte2
+	print pos_patte3
+	print pos_patte4
+	print pos_patte5
+	print pos_patte6
+	
+	#~ setPositionToLeg(pos_patte1[0], pos_patte1[1], pos_patte1[2], robot.patte_1)
+	#~ setPositionToLeg(pos_patte2[0], pos_patte2[1], pos_patte2[2], robot.patte_2)
+	#~ setPositionToLeg(pos_patte3[0], pos_patte3[1], pos_patte3[2], robot.patte_3)
+	#~ setPositionToLeg(pos_patte4[0], pos_patte4[1], pos_patte4[2], robot.patte_4)
+	#~ setPositionToLeg(pos_patte5[0], pos_patte5[1], pos_patte5[2], robot.patte_5)
+	#~ setPositionToLeg(pos_patte6[0], pos_patte6[1], pos_patte6[2], robot.patte_6)
+	time.sleep(1)
+	
+def marche(robot):
+	i=0
+	while i != 5:
+		moveRobot(0,0,30,robot, 1)
+		if i != 0:
+			moveRobot(-40,0,0,robot, 1)
+		moveRobot(40,0,0,robot, 2)
+		moveRobot(0,0,-30,robot, 1)
+		moveRobot(0,0,30,robot, 2)
+		moveRobot(-40,0,0,robot, 2)
+		moveRobot(40,0,0,robot, 1)
+		moveRobot(0,0,-30,robot, 2)
+		i = i+1
+		
+def rotation(robot):
+	i=0
+	rotateRobot(10,robot, 2)
+	#~ while i != 1:
+		#~ moveRobot(0,0,30,robot, 1)
+		#~ if i != 0:
+			#~ rotateRobot(-10,robot, 1)
+		#~ rotateRobot(10,robot, 2)
+		#~ moveRobot(0,0,-30,robot, 1)
+		#~ moveRobot(0,0,30,robot, 2)
+		#~ rotateRobot(-10,robot, 2)
+		#~ rotateRobot(10,robot, 1)
+		#~ moveRobot(0,0,-30,robot, 2)
+		#~ i = i+1
 
 if __name__ == '__main__':
 	
@@ -121,30 +227,10 @@ if __name__ == '__main__':
 		allMotorNotCompliant(robot)
 		allMotorInitialPos(robot)
 		time.sleep(1)
-		while True:
-			moveRobot(20,0,0,robot)
-			#~ setPositionToLeg(pos_patte1[0], pos_patte1[1]-20, pos_patte1[2], robot.patte_1)
-			#~ setPositionToLeg(pos_patte2[0]-20, pos_patte2[1], pos_patte2[2], robot.patte_2)
-			#~ setPositionToLeg(pos_patte3[0]-20, pos_patte3[1], pos_patte3[2], robot.patte_3)
-			#~ setPositionToLeg(pos_patte4[0], pos_patte4[1]+20, pos_patte4[2], robot.patte_4)
-			#~ setPositionToLeg(pos_patte5[0]+20, pos_patte5[1], pos_patte5[2], robot.patte_5)
-			#~ setPositionToLeg(pos_patte6[0]+20, pos_patte6[1], pos_patte6[2], robot.patte_6)
-			time.sleep(1)
-			moveRobot(-20,0,0,robot)
-			#~ setPositionToLeg(pos_patte1[0], pos_patte1[1]+20, pos_patte1[2], robot.patte_1)
-			#~ setPositionToLeg(pos_patte2[0]+20, pos_patte2[1], pos_patte2[2], robot.patte_2)
-			#~ setPositionToLeg(pos_patte3[0]+20, pos_patte3[1], pos_patte3[2], robot.patte_3)
-			#~ setPositionToLeg(pos_patte4[0], pos_patte4[1]-20, pos_patte4[2], robot.patte_4)
-			#~ setPositionToLeg(pos_patte5[0]-20, pos_patte5[1], pos_patte5[2], robot.patte_5)
-			#~ setPositionToLeg(pos_patte6[0]-20, pos_patte6[1], pos_patte6[2], robot.patte_6)
-			time.sleep(1)
 		
-		#~ moveOn(robot)
-		#~ setPositionToLeg(150,0,-30,robot.patte_3)
-		#~ for m in robot.motors:	
-			#~ print 'Current pos:', m.id, ' ', m.present_position
-		
-		
+		marche(robot)
+		#~ rotation(robot)
+			
 		#~ allMotorCompliant(robot)
 		pass
 		
